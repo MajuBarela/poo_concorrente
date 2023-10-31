508edf549c030503432590cc27c4ec3a8a83b2cd
CajuBarela <maria.netto01@etec.sp.gov.br>
2023-10-31 14:07:39 +0000

    Salvando processo

--- Principal.java
+++ Principal.java
@@ -3,5 +3,7 @@ public class Principal {
     public static void main(String[] args) {
         Processo proc1 = new Processo("Leo", 17);
         Processo proc2 = new Processo("Yara", 17);
+        proc1.start();
+        proc2.start();
     }
 }
